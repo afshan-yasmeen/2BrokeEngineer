f536b41c45a1e7255268725fdb2cdd93105f4135
AfshanYasmeen16 <afshanyasmeen16@gmail.com>
2023-12-24 16:04:00 +0500

    test

--- signUp.php
+++ signUp.php
@@ -14,12 +14,15 @@ if($conn->connect_error){
     die('Connection Failed'.$conn->connect_error );
 }
 else{
-    $stmt=$conn->prepare("insert into signUp(username,password ) values(?,?)");
+    $stmt=$conn->prepare("INSERT INTO signUp(username,password ) values(?,?)");
     $stmt->bind_param("ss",$username,$password);
     $stmt->execute();
-    header("Location: ./signIn.html");
+    echo "Insert success";
+   // header("Location: ./signIn.html");
     $stmt->close();
     $conn->close();
+
+    die();
 }
 }
 ?>
