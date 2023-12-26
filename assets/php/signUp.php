b25c52fa20f8e3f1ff30887a1d2d6f8b1a35cd98
Afshan Yasmeen <115390656+afshan-yasmeen@users.noreply.github.com>
2023-12-26 11:35:46 +0500

    Revert "New code"

---
+++ assets/php/signUp.php
@@ -0,0 +1,19 @@
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+echo `$username`;
+
+// Database Connection 
+include "../../connection/db.php";
+if($conn->connect_error){
+    die('Connection Failed'.$conn->connect_error );
+}
+else{
+    $stmt=$conn->prepare("insert into signUp(username,password ) values(?,?)");
+    $stmt->bind_param("ss",$username,$password);
+    $stmt->execute();
+    header("Location: ./signIn.html");
+    $stmt->close();
+    $conn->close();
+}
+?>
